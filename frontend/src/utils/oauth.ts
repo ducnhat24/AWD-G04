7856f18b23499f764042fbc9ed5ab6775329435a
ducnhat24 <ducnhaat@gmail.com>
2025-11-30 02:26:21 +0700

    feat(feat/frontend/google-oauth): gmail scope added

--- frontend/src/utils/oauth.ts
+++ frontend/src/utils/oauth.ts
@@ -10,6 +10,9 @@ export const getGoogleAuthUrl = () => {
         scope: [
             "https://www.googleapis.com/auth/userinfo.profile",
             "https://www.googleapis.com/auth/userinfo.email",
+            "https://www.googleapis.com/auth/gmail.readonly",
+            "https://www.googleapis.com/auth/gmail.send",
+            "https://www.googleapis.com/auth/gmail.modify"
         ].join(" "),
     };
 
